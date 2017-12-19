3cbc5e72333926679eb33c6aa0a8a516d7a0751c
Serge Aleksandrov <alserge.dct@gmail.com>
2017-12-19 14:57:50 -0500

    Innitialisation

---
+++ src/MainClass.java
@@ -0,0 +1,25 @@
+/*
+* Author is: Serge Aleksandrov
+* Date: 12/18/2017
+*
+* Class #1
+* */
+
+public class MainClass {
+  public static void main(String[] args) {
+    //This is one line comment
+    /*
+    * This is multi-line comment
+    *
+    * */
+    /*
+    * This method creates web-driver
+    * goes to site http://www.wix.com
+    * checks it's title
+    * */
+    /*
+    * This method logs custom test
+    * */
+    System.out.println("Hello, my name is Serge.");
+  }
+}
