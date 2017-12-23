a2add29677549b45d3399657895aab401fc8514d
Serge Aleksandrov <alserge.dct@gmail.com>
2017-12-22 19:56:58 -0500

    SquareBoard

---
+++ src/SquareBoard.java
@@ -0,0 +1,15 @@
+public class SquareBoard {
+
+  public static void main(String[] args) {
+    letsDoIt(5);
+  }
+
+  public static void  letsDoIt(int numberOfLoops) {
+    for (int x = 1; x <= 5; x++) {
+      for (int n = 1; n <= numberOfLoops; n++) {
+        System.out.print("#");
+      }
+      System.out.println();
+    }
+  }
+}
