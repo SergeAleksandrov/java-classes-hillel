7a507795680b70ee35cb5c0715207cb96449c38a
Serge Aleksandrov <alserge.dct@gmail.com>
2017-12-23 12:02:02 -0500

    WaterMixture (corrections 2)

--- src/HOME_TASKS/Lesson_3/WaterMixture.java
+++ src/HOME_TASKS/Lesson_3/WaterMixture.java
@@ -2,10 +2,10 @@ package HOME_TASKS.Lesson_3;
 
 public class WaterMixture {
   public static void main(String[] args) {
-    double volume = volume(2.5, 4.3);
-    double temperature = temperature(10, 7);
-    System.out.println("Volume of water mixture is " + volume(2.5, 4.3));
-    System.out.println("Temperature of water mixture is " + temperature(10, 7));
+    double volume = volume(2.5, 4.8);
+    double temperature = temperature(10, 8);
+    System.out.println("Volume of water mixture is " + volume);
+    System.out.println("Temperature of water mixture is " + temperature);
   }
   public static double volume(double v1, double v2){
     return v1 + v2;
