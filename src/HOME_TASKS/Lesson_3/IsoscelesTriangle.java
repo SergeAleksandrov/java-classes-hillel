d80bc2a04df7de99c80f857d30096c8a782aa633
Serge Aleksandrov <alserge.dct@gmail.com>
2017-12-23 20:46:43 -0500

    IsoscelesTriangle

---
+++ src/HOME_TASKS/Lesson_3/IsoscelesTriangle.java
@@ -0,0 +1,16 @@
+package HOME_TASKS.Lesson_3;
+
+public class IsoscelesTriangle {
+
+  public static void main(String[] args) {
+    int a = 1;
+    int b = 3;
+    int c = 3;
+
+    if (a == b || a == c || b == c) {
+      System.out.println("This is the isosceles triangle");
+    } else {
+      System.out.println("This is not the isosceles triangle");
+    }
+  }
+}
