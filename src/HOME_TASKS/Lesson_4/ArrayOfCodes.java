d5b3251c89264ed44401b80b7953b0c874a55f51
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-06 01:54:28 -0500

    ArrayOfCodes (new version)

--- src/HOME_TASKS/Lesson_4/ArrayOfCodes.java
+++ src/HOME_TASKS/Lesson_4/ArrayOfCodes.java
@@ -4,14 +4,16 @@ public class ArrayOfCodes {
 
   public static void main(String[] args) {
     char[] charArray = {'j', 'a', 'v', 't'};
-    int[] intArray = new int[4];
-    for (int i = 0; i < charArray.length; i++) {
-      int temp = (int) charArray[i];
-      intArray[i] = temp;
+    int[] intArray = new int[charArray.length];
+    for (int i = 0; i < intArray.length; i++) {
+      intArray[i] = (int) charArray[i];
     }
-    for (int l : intArray){
+    for (int l : intArray) {
       System.out.print(l + " ");
 
     }
+
   }
 }
+
+
