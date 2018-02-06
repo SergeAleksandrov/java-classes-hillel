c7382f1a8f2b2dcbf808a6c4a20ce2129084292d
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-06 15:21:15 -0500

    RandomlyGeneretedString

--- src/Infrastructure/EnvVar.java
+++ src/Infrastructure/EnvVar.java
@@ -5,7 +5,6 @@ public class EnvVar {
   public static void main(String[] args) {
     String browser = getEnvironmentVariableOrDefault("browserType", "Chrome");
     System.out.println(browser);
-
   }
 
   public static String getEnvironmentVariableOrDefault(String envVar, String defaultValue){
---
+++ src/Infrastructure/RandomlyGeneretedString.java
@@ -0,0 +1,36 @@
+package Infrastructure;
+
+import javax.sound.midi.Soundbank;
+
+public class RandomlyGeneretedString {
+
+  public static void main(String[] args) {
+    System.out.println(random("alpha", 10));
+    System.out.println(random("numeric", 10));
+    System.out.println(random("alphanumeric", 10));
+
+  }
+
+  public static String random (String mode, int length){
+    String alphabet;
+    switch (mode){
+      case "alpha":
+        alphabet = "abcdefghijklmnopqrstuvwxys";
+        break;
+      case "numeric":
+        alphabet = "0123456789";
+        break;
+      case "alphanumeric":
+        alphabet = "abcdefghijklmnopqrstuvwxys0123456789";
+        break;
+        default:
+         alphabet = "";
+    }
+    String result = "";
+    for (int i=0; i<length; i++){
+      int index = (int) (Math.random()*alphabet.length());
+      result += alphabet.charAt(index);
+    }
+    return result;
+  }
+}
