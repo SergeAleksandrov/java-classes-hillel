e16d8e39eb9eb6f33cf27c5b4aa81b45efb7b374
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-06 01:36:02 -0500

    ArraySum (2nd variant)

--- src/HOME_TASKS/Lesson_4/ArraySum.java
+++ src/HOME_TASKS/Lesson_4/ArraySum.java
@@ -3,14 +3,14 @@ package HOME_TASKS.Lesson_4;
 public class ArraySum {
 
   public static void main(String[] args) {
-    System.out.println("Array's sum is " +sum());
+    int[] array = {1,2,3,4,5,7,8};
+    System.out.println("Array's sum is " + sum(array));
   }
 
-  public static int sum (){
-    int[] array = {1,2,3,4,5,7};
+  public static int sum (int[] array){
     int sum = 0;
     for (int l : array){
-      sum = sum + l;
+      sum += l;
     }
     return sum;
   }
