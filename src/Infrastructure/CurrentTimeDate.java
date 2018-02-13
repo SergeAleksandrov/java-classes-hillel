4f9685aa5f8148faee2f86db5162512f3c5e1f4f
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-13 10:48:07 -0500

    CurrentTimeDate (new)

--- src/Infrastructure/CurrentTimeDate.java
+++ src/Infrastructure/CurrentTimeDate.java
@@ -6,9 +6,15 @@ import java.util.Date;
 public class CurrentTimeDate {
   public static void main(String[] args) {
     String time = "HH:mm:ss:SSS";
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
-    String date = simpleDateFormat.format(new Date());
-    System.out.println(date);
+    String date = "yyy-MM-dd";
+
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(time);
+    String currentTime = simpleDateFormat.format(new Date());
+    System.out.println(currentTime);
+
+    SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat(date);
+    String currentDate = simpleDateFormat2.format(new Date());
+    System.out.println(currentDate);
   }
 }
 
