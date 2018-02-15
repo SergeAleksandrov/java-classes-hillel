b5a3e6671f503ebf712c1d2926eb92a8066d8e54
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-15 15:59:08 -0500

    Choosing of browser with a help of ternary operator

--- src/Infrastructure/Configuration.java
+++ src/Infrastructure/Configuration.java
@@ -1,13 +1,4 @@
 package Infrastructure;
 
 public class Configuration {
-
-  public static void main(String[] args) {
-    String envVar = "browserType";
-    String browser = System.getenv("browserType") != null ? System.getenv("browserType") : "Chrome";
-    System.out.println("Browser is " + browser);
-    //System.out.println(System.getenv(args[0]));
-
-  }
-
 }
---
+++ src/Infrastructure/TernaryOperatorEnvVar.java
@@ -0,0 +1,14 @@
+package Infrastructure;
+
+public class TernaryOperatorEnvVar {
+
+  public static void main(String[] args) {
+    // Choosing of browser with a help of ternary operator 
+    String envVar = "browserType";
+    String browser = System.getenv("browserType") != null ? System.getenv("browserType") : "Chrome";
+    System.out.println("Browser is " + browser);
+    //System.out.println(System.getenv(args[0]));
+
+  }
+
+}
