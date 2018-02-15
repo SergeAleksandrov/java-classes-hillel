11e99441fec3f6b5ef6513c1057d477250e8f577
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-15 14:47:56 -0500

    Class Author and Equals() and Hash() classes

--- src/HOME_TASKS/Lesson_8/Author.java
+++ src/HOME_TASKS/Lesson_8/Author.java
@@ -25,6 +25,25 @@ public class Author {
     this.lastname = lastname;
   }
 
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Author author = (Author) o;
+
+    if (!name.equals(author.name)) return false;
+    return lastname.equals(author.lastname);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = name.hashCode();
+    result = 31 * result + lastname.hashCode();
+    return result;
+  }
+
   @Override
   public String toString() {
     return "Author{" + "name='" + name + '\'' + ", lastname='" + lastname + '\'' + '}';
--- src/HOME_TASKS/Lesson_8/LibApp.java
+++ src/HOME_TASKS/Lesson_8/LibApp.java
@@ -3,15 +3,23 @@ package HOME_TASKS.Lesson_8;
 public class LibApp {
   public static void main(String[] args) {
     Author author = new Author("John", "Man");
-    Book bookMy = new Book("234", "Mumu", author, "1900", "Story", "0000");
-    System.out.println(bookMy.getId());
-    System.out.println(bookMy.toString());
+    Book myBook = new Book("234", "Mumu", author, "1900", "Story", "0000");
+    System.out.println(myBook.getId());
+    System.out.println(myBook.toString());
 
-    Book bookMy2 = new Book("Mumu", author, "1900", "Story", "0000");
-    System.out.println(bookMy2.toString());
+    Book myBook2 = new Book("Mumu", author, "1900", "Story", "0000");
+    System.out.println(myBook2.toString());
 
-    Book bookMy3 = new Book();
-    System.out.println(bookMy3);
+    Book myBook3 = new Book();
+    System.out.println(myBook3);
+
+    Book myBook4 = new Book("234", "Mumu", author, "1900", "Story", "0000");
+    System.out.println(myBook4);
+
+
+    System.out.println(myBook.equals(myBook4));
+
+
 
   }
 }
