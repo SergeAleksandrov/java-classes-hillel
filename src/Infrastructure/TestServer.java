a4f5e970fb99ae560c260b1141a1d9934b2423cf
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-16 11:37:05 -0500

    TestServer

--- src/Infrastructure/TestServer.java
+++ src/Infrastructure/TestServer.java
@@ -1,4 +1,21 @@
 package Infrastructure;
 
 public class TestServer {
+
+  public String getUrl (){
+    String server = ConfigurationManager.getInstance().getTestBrowser();
+
+    switch (server){
+      case "local":
+        return "http://192.168.0.15";
+      case "staging":
+        return "http://staging.website.com:5678";
+      case "QA":
+        return "http://qa.website.com:5670";
+      case "production":
+        return "http://website.com";
+      default:
+        return "Invalid";
+    }
+  }
 }
